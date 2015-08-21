b79bbf8dfeb15e74abdcdc4527804bdd77768b7c
Mario Alvarez <mario.alvarez739@gmail.com>
2015-08-21 15:52:38 +0100

    Corrected comment

--- Arithmetic/Tests/Int/Incorrect.hs
+++ Arithmetic/Tests/Int/Incorrect.hs
@@ -1,18 +1,14 @@
 module Arithmetic.Tests.Int.Incorrect where
 
--- This fails because either number might be negative
 addition_monotonic :: Int -> Int -> Bool
 addition_monotonic i j = i + j > i
 
--- This fails because overflow
 addition_overflow :: Int -> Bool
 addition_overflow i = i + 1 > i
 
--- This fails because i might be negative
 multiplication_monotonic :: Int -> Bool
 multiplication_monotonic i = 2 * i >= i
 
--- This fails because 2i might overflow NEVERMIND IT ACTUALLY DOESN'T FAIL
 multiplication_overflow :: Int -> Bool
 multiplication_overflow i = i <= 0 || i < 2 * i
 
